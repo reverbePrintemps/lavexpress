680efc6fc7e8d6df01fe1dd1459f398de7164d48
jandreo <jonathan.andreo@soundcloud.com>
2020-05-09 18:04:01 +0200

    Essai sticky section titles

--- Personal/lavexpress/App/app.js
+++ Personal/lavexpress/App/app.js
@@ -1,8 +1,30 @@
 // TODO
-// TODO Bubbles
-// TODO Sticky and smooth scrolling?
 
-console.clear()
+// When the user resizes the page, execute myFunction
+window.onresize = function () {
+  myFunction();
+};
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  // Get the offset position of the navbar
+  var sticky = header.offsetTop;
+  var headerHeight = $(header).height();
+  if (window.pageYOffset < headerHeight) {
+    header.classList.remove("sticky");
+  } else if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  }
+}
+
+// —————————————— PARTICLES ——————————————
 
 particlesJS("particles-js", {
   particles: {
@@ -12,24 +34,24 @@ particlesJS("particles-js", {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
       polygon: { nb_sides: 5 },
-      image: { src: "img/github.svg", width: 100, height: 100 }
+      image: { src: "img/github.svg", width: 100, height: 100 },
     },
     opacity: {
       value: 0.5,
       random: true,
-      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
     },
     size: {
       value: 10,
       random: true,
-      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
     },
     line_linked: {
       enable: false,
       distance: 500,
       color: "#ffffff",
       opacity: 0.4,
-      width: 2
+      width: 2,
     },
     move: {
       enable: true,
@@ -39,23 +61,23 @@ particlesJS("particles-js", {
       straight: false,
       out_mode: "out",
       bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 }
-    }
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
   },
   interactivity: {
     detect_on: "canvas",
     events: {
       onhover: { enable: true, mode: "bubble" },
       onclick: { enable: true, mode: "repulse" },
-      resize: true
+      resize: true,
     },
     modes: {
       grab: { distance: 400, line_linked: { opacity: 0.5 } },
       bubble: { distance: 400, size: 4, duration: 0.3, opacity: 1, speed: 3 },
       repulse: { distance: 200, duration: 0.4 },
       push: { particles_nb: 4 },
-      remove: { particles_nb: 2 }
-    }
+      remove: { particles_nb: 2 },
+    },
   },
-  retina_detect: true
+  retina_detect: true,
 });
